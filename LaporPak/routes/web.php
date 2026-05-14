5a3142623f394826322a15ed9ec9bfbd64f96542
Zahwatya <zahwafinif@gmail.com>
2026-05-14 17:22:27 +0800

    update

--- LaporPak/routes/web.php
+++ LaporPak/routes/web.php
@@ -1,20 +1,23 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PengaduanController; // <-- Tambahkan ini
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
+// Route Group khusus yang sudah login
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // --- ROUTES WARGA ---
+    Route::get('/riwayat-laporan', [PengaduanController::class, 'index'])->name('pengaduan.index');
+    Route::get('/buat-laporan', [PengaduanController::class, 'create'])->name('pengaduan.create');
+    Route::post('/buat-laporan', [PengaduanController::class, 'store'])->name('pengaduan.store');
 });
 
 require __DIR__.'/auth.php';
